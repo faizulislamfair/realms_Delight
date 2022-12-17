b0578c9fefe9e89180a78bdd45468fccfa917819
faizulislamfair <faizulislamfair@gmail.com>
2022-12-17 21:06:43 +0600

    Head component tab title, seo

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,13 @@
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
 const Blog = () => {
     return (
         <div>
+            <Head>
+                <title>Blog Page</title>
+            </Head>
+
             <h1>This is blog page</h1>
             <Link href="/blog/blog1">
                 Blog Page 1
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+
       <h1>This is Next.js</h1>
       <Link href="/blog">
         <h5 style={{ marginTop: '55px' }} >Blog</h5>
