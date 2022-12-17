4d24821d34f84cb50ad4c75370b59c2e26c035d8
faizulislamfair <faizulislamfair@gmail.com>
2022-12-17 20:19:59 +0600

    dynamic routes

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+const blogId = () => {
+
+    const router = useRouter();
+    const id = router.query.blogId;
+
+    return (
+        <div>
+            <h1>This is a dynamic page of {id}</h1>
+        </div>
+    );
+};
+
+export default blogId;
