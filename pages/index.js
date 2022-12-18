93f8913f69664531b5d2c2e034956bc434dea763
faizulislamfair <faizulislamfair@gmail.com>
2022-12-19 00:05:35 +0600

    stuffs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Banner from './../Components/Banner/Banner';
+import Navbar from './../Components/Navbar/Navbar';
+import Footer from './../Components/Footer/Footer';
+
 export default function Home() {
   return (
     <div>
+      <Navbar></Navbar>
+      <Banner></Banner>
 
+      <Footer></Footer>
     </div >
   )
 }
---
+++ pages/news/index.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/News.module.css';
+
+const News = () => {
+    return (
+        <div className={styles.box}>
+            <h1>News</h1>
+        </div>
+    );
+};
+
+export default News;
